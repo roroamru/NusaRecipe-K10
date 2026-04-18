aae3d420cb12cb8e53f9a2ba3fa24010d88dbb87
abharroafazahida <abharroafaz@gmail.com>
2026-04-18 16:39:14 +0700

    menambah logo nusarecipe

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar bg-light shadow-sm px-4">
     <div class="container d-flex justify-content-between align-items-center">
 
-        <!-- LOGO -->
+        <!-- LOGO + NAMA -->
         <div class="d-flex align-items-center gap-2">
-            <img src="{{ asset('images/logo.png') }}" style="width:45px;">
+            <img src="{{ asset('image/logo.png') }}" alt="logo" style="width:45px;">
             <h4 class="fw-bold text-warning m-0">NusaRecipe</h4>
         </div>
 
@@ -21,11 +21,13 @@
         <!-- MENU -->
         <div class="d-flex align-items-center gap-4">
 
+            <!-- FAVORIT -->
             <div class="text-center">
                 <i class="bi bi-bookmark fs-4 text-warning"></i>
                 <div style="font-size:12px;">Favorit</div>
             </div>
 
+            <!-- USER -->
             <i class="bi bi-person-circle fs-3 text-warning"></i>
 
         </div>
